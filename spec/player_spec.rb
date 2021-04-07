f45187d868158d40d903037b5566af98cc5944bb
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-07 09:15:24 -0600

    Create position method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,15 @@
 require './lib/player'
 
 describe Player do
-  it 'can have a name' do
+  it 'has a name' do
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
 
     expect(roy.name).to eq("Roy Kent")
+  end
+
+  it 'has a position' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
+
+    expect(roy.position).to eq("Center Midfielder")
   end
 end
