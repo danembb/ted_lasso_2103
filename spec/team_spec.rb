309fe2f79c2320e0804ff9d8f493a7d27ffb23e6
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-07 09:30:18 -0600

    Create coach method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -6,7 +6,15 @@ describe Team do
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
     sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
     richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-    
+
     expect(richmond.name).to eq("AFC Richmond")
+  end
+
+  it 'has a coach' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond.coach).to eq("Ted Lasso")
   end
 end
