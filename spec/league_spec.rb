9670bb9fafa683859559d72d2dbe85f7a8f6d797
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-07 10:54:07 -0600

    Create teams method with no teams by default

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -8,4 +8,10 @@ describe League do
 
     expect(premier.name).to eq("Premier League")
   end
+
+  it 'has no teams by default' do
+    premier = League.new("Premier League")
+
+    expect(premier.teams).to eq []
+  end
 end
